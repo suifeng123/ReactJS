bd74c9fa40aeaa2623b68b37b0e9a638957329e5
wangshengwen <wswen1220@163.com>
2017-05-25 14:21:16 +0800

    ReactJS

--- app5/src/routes/index.js
+++ app5/src/routes/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import { Router, Route, IndexRoute } from 'react-router';
+import { Router, Route, IndexRoute,hashHistory } from 'react-router';
 
-import Frame from '../layouts/Frame';
 import Home from '../views/Home';
 import Detail from '../views/Detail';
 
-const routes = browserHistory => (
-  <Router history={browserHistory}>
-    <Route path="/" component={Frame}>
-      <IndexRoute component={Home} />
-      <Route path="/detail/:id" component={Detail} />
-    </Route>
-  </Router>
+const routes = (
+    <Router history={hashHistory}>
+        <Route path="/" component={Home} />
+        <Route path="/detail/:id" component={Detail} />
+    </Router>
 );
 
 export default routes;
