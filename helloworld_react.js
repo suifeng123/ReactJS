77a0a849cbe42bb0c2a2bbe89e5e17dc7fb764ac
suifeng123 <wswen1220@163.com>
2017-09-04 18:16:47 +0800

    提交一版最新的代码

--- helloworld_react.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Administrator on 2017/5/9.
- */
-ReactDOM.render(
-    <h1>Hello wolrd</h1>,
-    document.getElementById('example')
-)
--- test.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Administrator on 2017/5/19.
- */
-(function(){
-
-    //添加
-})(window)
