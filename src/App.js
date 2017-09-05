13cdf573c7c31ad513efe59a7b83786ef1be09ee
suifeng123 <wswen1220@163.com>
2017-09-05 15:59:10 +0800

    安装了antd,并修改部分代码

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Layout } from 'antd';
 import './App.css';
+const { Content,Footer } = Layout; //进行解构
 
 class App extends Component {
+  state = {
+    collapsed: false,
+  };
+  toggle = () => {
+      //调用函数进行设置状态
+  };
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+         
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
