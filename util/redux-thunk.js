eefec8eefe47f8e0841679fdc9839b0d78ff0d3b
suifeng123 <wswen1220@163.com>
2017-09-14 16:26:56 +0800

    提交一个插件的算法

--- util/redux-thunk.js
+++ util/redux-thunk.js
@@ -10,3 +10,32 @@ function createThunkMiddleware(extraArgument){
 
 const thunk = createThunkMiddleware();
 export default thunk;
+
+function isPromise(obj){
+	return obj && obj.then === 'function';
+}
+
+export default function promiseMiddleware({dispatch}){
+	return function(next){
+		return function(action){
+			return isPromise(action) ? action.then(dispatch):next(action);
+		}
+	}
+}
+
+export default function promiseMiddleware({dispatch}){
+	return (next) => (action) => {
+		const {types,promise,...rest} = action;
+		if(!isPromise(promise) || !(action.types && action.types.length === 3)){
+			return next(action);
+		}
+
+		const [PENDING,DONE,FAIL] = types;
+
+		dispatch({...rest,type:PENDING});
+		return action.promise.then(
+          (result) => dispatch({...rest,result,type:DONE}),
+          (error) => dispatch({...rest,error,type:FAIL})
+		);
+	};
+}
